b1bc48cbb12e34c993ed878132f18c884280dfc8
AnaniyaLegesse <ananiya.legesse1@gmail.com>
2025-11-11 16:37:22 +0300

    Getting started

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+
+
+
+
+
+
+const page = () => {
+
+    
+  return (
+    <div className="min-h-screen bg-gray-50 font-sans antialiased">
+ 
+    </div>
+  );
+}
+
+export default page
